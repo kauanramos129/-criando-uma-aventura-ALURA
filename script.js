19eaa629365a4c37c5bf84e0df10f8272184f095
kauanramos129 <andrade.ramos.kauan@escola.pr.gov.br>
2024-11-14 08:35:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
-        const atual = document.querySelectorAll('.ativo');
-        const proximoPaso = 'passo-' + this.getAttribute('data-proximo');
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        \TextTrackCueList.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo')
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
 })
